084ac31835c7a4a42f661dc538ca6ff8ed68dd41
Stephen <stephen.mizon@gmail.com>
2024-01-10 23:04:43 +0000

    added sonarQube

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0l$!y2*0yho0uicfxekqtrl8j)7x8xv8s@q%h6x00j1p(lacz*'
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-0l$!y2*0yho0uicfxekqtrl8j)7x8xv8s@q%h6x00j1p(lacz*")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,3 +129,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
